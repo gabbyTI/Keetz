8438626d8fa157e19c19d3698622b61a94318b6b
Gabriel <gabrielibenye@gmail.com>
2018-09-03 21:29:06 +0100

    added client and admin side of the projects

---
+++ Admin Side/KeetzAdmin/KeetzAdmin/Models/TemData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KeetzAdmin.Models
+{
+    public static class TemData
+    {
+        public static int EmpID { get; set; }
+    }
+}
---
+++ Client Side/Keetz/Keetz/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Keetz.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Keetz.Controllers
+{
+    public class HomeController : Controller
+    {
+        KeetzEntities db = new KeetzEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.MenProduct = db.Products.Where(x => x.Category.Name.Equals("Men")).ToList();
+            ViewBag.WomenProduct = db.Products.Where(x => x.Category.Name.Equals("Women")).ToList();
+            ViewBag.SportsProduct = db.Products.Where(x => x.Category.Name.Equals("Sports")).ToList();
+            ViewBag.ElectronicsProduct = db.Products.Where(x => x.Category.Name.Equals("Phones")).ToList();
+            ViewBag.Slider = db.genMainSliders.ToList();
+            ViewBag.PromoRight = db.genPromoRights.ToList();
+
+            this.GetDefaultData();
+
+            return View();
+        }      
+
+    }
+}
